defbc891e4524ee26eee476a001c3ada5b2dccad
kentozzz <kento.sobayashi@di-v.co.jp>
2024-02-10 14:42:29 +0900

    一覧ページの土台作成

--- src/components/TweetIndex.vue
+++ src/components/TweetIndex.vue
@@ -1,7 +1,10 @@
 <!-- TweetIndex.vue -->
 <template>
-  <div>
-    <!-- ここに任意のマークアップを追加 -->
-    <!-- <p>{{ message }}</p> -->
+  <div class="contents row">
+  <!-- <div class="content_post" style="background-image: url("ツイートの画像");"> -->
+    <p>ツイートの本文</p>
+    <span class="name">
+      投稿者名
+    </span>
   </div>
 </template>
